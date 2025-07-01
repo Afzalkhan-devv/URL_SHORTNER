81b9aa5446c7485806181ba1325b6e57693397c2
Afzal khan <Afzalkhan6673@gmail.com>
2025-07-01 17:14:17 +0530

    commit code

--- urlshortner/src/main/java/com/example/java/urlshortner/HomeController.java
+++ urlshortner/src/main/java/com/example/java/urlshortner/HomeController.java
@@ -1,35 +1,39 @@
 package com.example.java.urlshortner;
  
+
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
- 
-import java.util.List;
- 
-@Controller
 
+import com.example.java.urlshortner.domain.entities.ShortUrl;
+import com.example.java.urlshortner.domain.repositories.ShortUrlRepository;
+import com.example.java.urlshortner.services.ShortUrlService;
+
+import java.util.List;
+@Controller
 public class HomeController {
- 
-    @GetMapping("/")
+	
+	private final ShortUrlService shortUrlService;
+	
+	public HomeController(ShortUrlService shortUrlService) {
+		this.shortUrlService = shortUrlService;
+	}
+
+
+	@GetMapping("/")
 
     public String home(Model model) {
-
-        model.addAttribute("title", "URL Shortener - Thymeleaf");
-
+		List<ShortUrl> shortUrls = shortUrlService.findAllPublicShortUrls();
+        model.addAttribute("shortUrls", shortUrls);
+        model.addAttribute("baseUrl","http://localhost:8080/");
         return "index";
 
     }
  
  
-    @GetMapping("/about")
-
-    public String about() {
-
-        return "about";
-
-    }
+  
 
 }
  
