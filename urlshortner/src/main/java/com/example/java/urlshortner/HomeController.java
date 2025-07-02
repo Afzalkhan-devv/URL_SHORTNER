34cd80422aa077d14ed8a82fa5f0a6ec6ccf5597
Afzal khan <Afzalkhan6673@gmail.com>
2025-07-02 22:22:11 +0530

    commit 4

--- urlshortner/src/main/java/com/example/java/urlshortner/HomeController.java
+++ urlshortner/src/main/java/com/example/java/urlshortner/HomeController.java
@@ -2,13 +2,10 @@ package com.example.java.urlshortner;
  
 
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
-
 import org.springframework.web.bind.annotation.GetMapping;
-
 import com.example.java.urlshortner.domain.entities.ShortUrl;
-import com.example.java.urlshortner.domain.repositories.ShortUrlRepository;
+import com.example.java.urlshortner.domain.model.ShortUrlDto;
 import com.example.java.urlshortner.services.ShortUrlService;
 
 import java.util.List;
@@ -25,7 +22,7 @@ public class HomeController {
 	@GetMapping("/")
 
     public String home(Model model) {
-		List<ShortUrl> shortUrls = shortUrlService.findAllPublicShortUrls();
+		List<ShortUrlDto> shortUrls = shortUrlService.findAllPublicShortUrls();
         model.addAttribute("shortUrls", shortUrls);
         model.addAttribute("baseUrl","http://localhost:8080/");
         return "index";
